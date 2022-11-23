cd622e248e6664b2f369f6cc2362df37a00bd489
dmitrybukhonov <d.buhonov@yandex.ru>
2022-11-23 20:26:48 +0400

    Task: #2 ход от карусели

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,6 @@
   <v-app>
     <header></header>
     <v-main>
-      <!-- <v-container>
-        <v-toolbar flat height="72">
-          <v-switch v-model="$vuetify.theme.dark" inset></v-switch>
-        </v-toolbar>
-      </v-container> -->
       <SheepCounter msg="Sheep Counter" />
       <FooterBlock />
     </v-main>
@@ -19,10 +14,6 @@ import FooterBlock from './FooterBlock.vue';
 
 export default {
   name: 'App',
-  metaInfo: {
-    title: 'Default App Title',
-    titleTemplate: '%s | vue-meta Example App'
-  },
   components: {
     SheepCounter,
     FooterBlock,
@@ -30,17 +21,4 @@ export default {
 }
 </script>
 
-<style>
-/* #app {
-  position: fixed;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-} */
-</style>
+
--- src/assets/vue-coverflow/lib/index.js
+++
@@ -1,7 +0,0 @@
-import CoverflowBlock from './CoverflowBlock.vue'
-
-CoverflowBlock.install = (Vue) => {
-  Vue.component(CoverflowBlock.name, CoverflowBlock)
-}
-
-export default CoverflowBlock
