6992aa25420d5824b523b7aaa83c9cde67042f39
dmitrybukhonov <d.buhonov@yandex.ru>
2023-06-07 23:08:41 +0400

    Task: #4 Актуализация линтера

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
-import Vue from 'vue'
-import App from './App.vue'
-import Buefy from 'buefy'
+import Vue from 'vue';
+import App from './App.vue';
+import Buefy from 'buefy';
 import '@/style/index.scss';
 
 Vue.use(Buefy);
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
